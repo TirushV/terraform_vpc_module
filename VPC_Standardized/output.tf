f68aaec0e99da24cd2c242061f449dd1edd04f29
TirushV <vtirush@gmail.com>
2022-10-24 13:23:41 +0530

    Updated Output.tf file

--- VPC_Standardized/output.tf
+++ VPC_Standardized/output.tf
@@ -1,31 +1,19 @@
-# output "aws_vpc_id" {
-#   value = aws_vpc.testing.id
-# }
+output "aws_vpc_id" {
+  value = aws_vpc.testing.id
+}
 
-# output "aws_vpc_cidr" {
-#   value = aws_vpc.testing.cidr_block
-# }
+output "aws_vpc_cidr" {
+  value = aws_vpc.testing.cidr_block
+}
 
-# output "public_subnet1" {
-#   value = aws_subnet.Public_subnet_1.id
-# }
+output "public_subnets" {
+  value = aws_subnet.Public_subnets[*].id
+}
 
-# output "public_subnet2" {
-#   value = aws_subnet.Public_subnet_2.id
-# }
+output "private_subnets" {
+  value = aws_subnet.Private_subnets[*].id
+}
 
-# output "public_subnet_3" {
-#   value = try(aws_subnet.Public_subnet_3[*].id, "")
-# }
-
-# output "private_subnet1" {
-#   value = aws_subnet.Private_subnet_1.id
-# }
-
-# output "private_subnet2" {
-#   value = aws_subnet.Private_subnet_2.id
-# }
-
-# output "private_subnet3" {
-#   value = aws_subnet.Private_subnet_3.id
-# }
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
